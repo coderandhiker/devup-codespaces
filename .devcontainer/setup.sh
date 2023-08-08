2ba8d0890bbc79e4a7774c95d5752d4865c2cca7
Chris Roberts <chrisrt@microsoft.com>
2023-08-08 15:51:38 +0000

    Added node and alias to setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,1 +1,10 @@
-pip3 install --user -r ./.devcontainer/requirements.txt
+# Install all the Python packages we'll need
+#pip3 install --user -r ./.devcontainer/requirements.txt
+
+# Install node/npx so we can create NextJS apps
+sudo apt install curl -y
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install nodejs
+
+# Set ll alias
+alias ll="ls -al"
